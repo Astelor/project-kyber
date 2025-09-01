a6db5b1dd6f51976b80711da31e2b5b1f9202d54
astelor <astelor000@gmail.com>
2025-09-01 17:23:28 +0800

    test .txt .hex files, dectohex.py script

--- test/dectohex.py
+++ test/dectohex.py
@@ -1,9 +1,10 @@
 def tohex(val, nbits):
     return hex((val + (1 << nbits)) % (1 << nbits))
 
-
-with open("zeta.txt", "r") as file:
-    with open("dumpster_fire","w") as write:
+# change the file name 
+# for the data needed to be converted in to HEX
+with open("test-invntt-out-no-mult.txt","r") as file:
+    with open("test-invntt-out-no-mult.hex","w") as write:
         line = 1
         while(line):
             line = file.readline();
