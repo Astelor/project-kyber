5cc510b4fb42bb3083d746730d47e43941fad2e8
astelor <astelor000@gmail.com>
2025-09-01 17:19:06 +0800

    basemul_tomont module that multiplies the entire polynomial

---
+++ basemul_tomont.v
@@ -0,0 +1,240 @@
+/*
+TODO: might pass a "ID" for calculation type (A*y or t*y etc), 
+      making controller easier to manage where the output should go
+*/
+module basemul_tomont #(parameter DEPTH = 8)(
+  // SYSTEM
+  input clk,
+  input set,
+  input reset,
+  // FSM
+  input readin_a,
+  input readin_b,
+  input readout,
+  input cal_en,
+  input full_in_a, // I guess? manage index ref externally
+  input full_in_b,
+  // INPUT TO RAMa
+  input wire signed [15:0] basemul_dina_1, // RAMa
+  input wire signed [15:0] basemul_dina_2, // RAMa
+  input wire [DEPTH-1:0] ina_index,
+  // INPUT TO RAMb
+  input wire signed [15:0] basemul_dinb_1, // RAMb
+  input wire signed [15:0] basemul_dinb_2, // RAMb
+  input wire [DEPTH-1:0] inb_index,
+
+  // OUTPUT TO OUTSIDE
+  output wire signed [15:0] basemul_dout_1,
+  output wire signed [15:0] basemul_dout_2,
+  output wire [DEPTH-1:0] out_index,
+
+  // misc
+  output wire readin_a_ok,
+  output wire readin_b_ok,
+  output wire done
+);
+
+reg [DEPTH-1:0] counter; 
+`define ZETA_THING (counter & 'b1)
+
+wire rama_we;
+wire ramb_we;
+wire ramc_we;
+reg [DEPTH-1:0] index_a;
+reg [DEPTH-1:0] index_b;
+reg [DEPTH-1:0] index_c;
+
+// FSM ================================================================
+wire iscal;
+wire index_a_ctrl;
+wire index_b_ctrl;
+wire index_c_ctrl;
+wire counter_ctrl;
+wire rama_we_ok;
+wire ramb_we_ok;
+wire ramc_we_ok;
+
+reg readin_a_ok_r;
+reg readin_b_ok_r;
+
+// wire -> assign addr1 = index, addr2 = index + offset
+// RAM A ==============================================================
+wire [DEPTH-1:0] rama_addr_1 = (iscal) ? index_a     : ina_index    , 
+                 rama_addr_2 = (iscal) ? index_a + 1 : ina_index + 1; 
+wire [15:0]      rama_din_1 = basemul_dina_1,
+                 rama_din_2 = basemul_dina_2;
+wire [15:0]      rama_dout_1, rama_dout_2;
+
+// RAM B ==============================================================
+wire [DEPTH-1:0] ramb_addr_1 = (iscal) ? index_b     : inb_index    , 
+                 ramb_addr_2 = (iscal) ? index_b + 1 : inb_index + 1;
+wire [15:0]      ramb_din_1 = basemul_dinb_1,
+                 ramb_din_2 = basemul_dinb_2;
+wire [15:0]      ramb_dout_1, ramb_dout_2;
+
+wire signed [15:0] t0;
+wire signed [15:0] t1;
+wire signed [15:0] a0 = rama_dout_1;
+wire signed [15:0] a1 = rama_dout_2;
+wire signed [15:0] b0 = ramb_dout_1;
+wire signed [15:0] b1 = ramb_dout_2;
+
+wire signed [15:0] zeta;
+wire signed [15:0] zeta_bm = `ZETA_THING ? zeta : -zeta;
+
+wire set_bm = iscal;
+
+reg  [6:0] k;
+wire [6:0] zeta_k = 64 + k; // zeta index
+
+// RAM C ==============================================================
+wire [DEPTH-1:0] ramc_addr_1 = index_c    , 
+                 ramc_addr_2 = index_c + 1;
+wire [15:0]      ramc_din_1  = t0,
+                 ramc_din_2  = t1;
+wire [15:0]      ramc_dout_1, ramc_dout_2;
+
+// TODO: forcing this "no leaking" by using readout flag maybe an overkill
+assign basemul_dout_1 = (done & readout) ? ramc_dout_1 : 0;
+assign basemul_dout_2 = (done & readout) ? ramc_dout_2 : 0;
+assign out_index      = (done) ? index_c - 2 : 0;
+
+dual_ram #(DEPTH, 16) rama(
+  .clk(clk),
+  .we_1  (rama_we),
+  .we_2  (rama_we),
+  .addr_1(rama_addr_1),
+  .addr_2(rama_addr_2),
+  .din_1 (rama_din_1),
+  .din_2 (rama_din_2),
+  .dout_1(rama_dout_1),
+  .dout_2(rama_dout_2)
+);
+
+dual_ram #(DEPTH, 16) ramb(
+  .clk(clk),
+  .we_1  (ramb_we),
+  .we_2  (ramb_we),
+  .addr_1(ramb_addr_1),
+  .addr_2(ramb_addr_2),
+  .din_1 (ramb_din_1),
+  .din_2 (ramb_din_2),
+  .dout_1(ramb_dout_1),
+  .dout_2(ramb_dout_2)
+);
+
+dual_ram #(DEPTH, 16) ramc(
+  .clk(clk),
+  .we_1  (ramc_we),
+  .we_2  (ramc_we),
+  .addr_1(ramc_addr_1),
+  .addr_2(ramc_addr_2),
+  .din_1 (ramc_din_1),
+  .din_2 (ramc_din_2),
+  .dout_1(ramc_dout_1),
+  .dout_2(ramc_dout_2)
+);
+
+basemul bm1(
+  .clk(clk),
+  .set(set_bm),
+  .a1(a1),
+  .a0(a0),
+  .b1(b1),
+  .b0(b0),
+  .zeta(zeta_bm),
+  .t1(t1),
+  .t0(t0)
+);
+
+// TODO: you only need one
+zeta_rom #(0) rom1(
+  .clk(clk),
+  .addr(zeta_k),
+  .data_out(zeta)
+);
+
+wire readin_a_ok_fsm;
+wire readin_b_ok_fsm;
+wire full_in_fsm = (~readin_a_ok_r) & (~readin_b_ok_r);
+wire cal_pulse;
+basemul_tomont_fsm #(DEPTH) fsm(
+  .clk(clk),
+  .set(set),
+  .reset(reset),
+  .readout(readout),
+  .cal_en(cal_en),
+  .full_in(full_in_fsm),
+  .counter(counter),
+
+  // OUTPUT
+  .iscal(iscal),
+  .index_a_ctrl(index_a_ctrl),
+  .index_b_ctrl(index_b_ctrl),
+  .index_c_ctrl(index_c_ctrl),
+  .counter_ctrl(counter_ctrl),
+
+  .rama_we_ok(rama_we_ok),
+  .ramb_we_ok(ramb_we_ok),
+  .ramc_we_ok(ramc_we_ok),
+
+  .readin_a_ok(readin_a_ok_fsm), // pulse
+  .readin_b_ok(readin_b_ok_fsm),
+  .cal_pulse(cal_pulse),
+  .done(done)
+);
+
+assign rama_we = rama_we_ok & (~iscal) & readin_a & readin_a_ok_r;
+assign ramb_we = ramb_we_ok & (~iscal) & readin_b & readin_b_ok_r;
+assign ramc_we = ramc_we_ok;
+
+assign readin_a_ok = readin_a_ok_r;
+assign readin_b_ok = readin_b_ok_r;
+
+always @(*)begin
+  readin_a_ok_r = (readin_a_ok_r | readin_a_ok_fsm) & (~full_in_a);
+  readin_b_ok_r = (readin_b_ok_r | readin_b_ok_fsm) & (~full_in_b);
+end
+
+always @(posedge clk or posedge reset) begin
+  if(reset) begin
+    index_a <= 0;
+    index_b <= 0;
+    index_c <= 0;
+    counter <= 0;
+    k <= 0;
+    //readin_a_ok_r <= 0;
+    //readin_a_ok_r <= 0;
+  end
+  else if (set & iscal) begin
+    if(index_a_ctrl) begin
+      index_a <= index_a + 2;
+    end
+    if(index_b_ctrl) begin
+      index_b <= index_b + 2;
+    end
+    if(index_c_ctrl) begin
+      index_c <= index_c + 2;
+    end
+    if(counter_ctrl) begin
+      counter <= counter + 1;
+    end
+    // TODO one index would suffice?
+    if(index_a_ctrl & counter_ctrl & `ZETA_THING) begin
+      k <= k + 1; // used only for the calculation
+    end
+  end
+  else if(set & (~iscal) ) begin
+    if(done & readout & index_c_ctrl) begin
+      index_c <= index_c + 2;
+    end
+    // use the readin_a_ok_fsm pulse to reset the counter?
+    if(cal_pulse)begin
+      counter <= 0; // use the pulse to reset the counter?
+      k <= 0; // k should reset itself if DEPTH = 8
+      index_c <= 0;
+    end
+  end
+end
+
+endmodule
---
+++ basemul_tomont_fsm.v
@@ -0,0 +1,206 @@
+module basemul_tomont_fsm #(parameter DEPTH = 8)(
+  input clk,
+  input set,
+  input reset,
+  /*
+  input readin,
+  */
+  input readout,
+  input cal_en,
+  input full_in,
+  //input full_out,
+  input [DEPTH-1:0] counter,
+
+  output reg iscal,
+  output reg index_a_ctrl,
+  output reg index_b_ctrl,
+  output reg index_c_ctrl,
+  output reg counter_ctrl,
+
+  output reg rama_we_ok,
+  output reg ramb_we_ok,
+  output reg ramc_we_ok,
+
+  output reg readin_a_ok,
+  output reg readin_b_ok,
+  output reg cal_pulse,
+  output reg done
+);
+
+// STATES
+parameter STAT_S0   = 1;
+parameter STAT_S0_1 = 2;
+parameter STAT_S1   = 3;
+parameter STAT_S2   = 4;
+parameter STAT_S2_1 = 5;
+parameter STAT_S3   = 6;
+parameter STAT_S3_1 = 7;
+
+parameter COMP_S0 = 10;
+parameter COMP_S1 = 11;
+parameter COMP_S2 = 12;
+
+reg [7:0] curr_state;
+reg [7:0] next_state;
+
+always @(posedge clk or posedge reset) begin
+  if(reset) begin
+    curr_state <= STAT_S0;
+  end
+  else begin
+    curr_state <= next_state;
+  end
+end
+
+// next state
+always @(*) begin
+  if(set) begin
+    case (curr_state)
+      STAT_S0 : begin // readin signal pulse
+        next_state = STAT_S0_1;
+      end
+      STAT_S0_1 : begin // no done flag
+        if(full_in)
+          next_state = STAT_S1;
+        else
+          next_state = STAT_S0_1;
+      end
+      STAT_S1 : begin
+        if(cal_en)
+          next_state = COMP_S0;
+        else
+          next_state = STAT_S1;
+      end
+      COMP_S0 : begin
+        if(counter == 4)
+          next_state = COMP_S1;
+        else 
+          next_state = COMP_S0;
+      end
+      COMP_S1 : begin
+        if(counter == (1 << (DEPTH - 1)) - 2)
+          next_state = COMP_S2;
+        else
+          next_state = COMP_S1;
+      end
+      COMP_S2 : begin 
+        if(counter == (1 << (DEPTH - 1)) + 4)
+          next_state = STAT_S2;
+        else
+          next_state = COMP_S2;
+      end
+      STAT_S2 : begin // readin signal pulse
+        next_state = STAT_S2_1;
+      end
+      STAT_S2_1 : begin // yes done flag
+        if(full_in)
+          next_state = STAT_S3;
+        else
+          next_state = STAT_S2_1;
+      end
+      STAT_S3 : begin
+        if(cal_en)
+          next_state = STAT_S3_1;
+        else
+          next_state = STAT_S3;
+      end
+      STAT_S3_1 : begin
+        next_state = COMP_S0;
+      end
+      default: begin
+        $display("[%0t] forbidden state", $time);
+      end 
+    endcase
+  end
+end
+
+// condition
+always @(posedge clk or posedge reset) begin
+  if(reset) begin
+    iscal <= 0;
+    done <= 0;
+    index_a_ctrl <= 0;
+    index_b_ctrl <= 0;
+    index_c_ctrl <= 0;
+    counter_ctrl <= 0;
+    rama_we_ok <= 0;
+    ramb_we_ok <= 0;
+    ramc_we_ok <= 0;
+    readin_a_ok <= 0;
+    readin_b_ok <= 0;
+    cal_pulse <= 0;
+  end
+  else if(set) begin
+    case (curr_state)
+      STAT_S0 : begin
+        iscal <= 0;
+        done <= 0;
+        rama_we_ok <= 1;
+        ramb_we_ok <= 1;
+        readin_a_ok <= 1;
+        readin_b_ok <= 1;
+
+        readin_a_ok <= 1; // pulse
+        readin_b_ok <= 1;
+
+      end
+      STAT_S0_1 : begin
+        readin_a_ok <= 0; // pulse
+        readin_b_ok <= 0;
+      end
+      STAT_S1 : begin
+        rama_we_ok <= 0;
+        ramb_we_ok <= 0;
+        cal_pulse   <= 1; // clearing index_c/counter/k
+      end
+      COMP_S0 : begin
+        iscal <= 1;
+        index_a_ctrl <= 1;
+        index_b_ctrl <= 1;
+        index_c_ctrl <= 0;
+        counter_ctrl <= 1;
+        rama_we_ok <= 0;
+        ramb_we_ok <= 0;
+        ramc_we_ok <= 0;
+        done <= 0;
+
+        cal_pulse <= 0;
+      end
+      COMP_S1 : begin
+        index_c_ctrl <= 1;
+        ramc_we_ok <= 1;
+      end
+      COMP_S2 : begin
+        index_a_ctrl <= 0;
+        index_b_ctrl <= 0;
+      end
+      STAT_S2 : begin
+        iscal <= 0;
+        index_c_ctrl <= 1; // letting readout controll index_c
+        counter_ctrl <= 0;
+
+        rama_we_ok <= 1;
+        ramb_we_ok <= 1;
+        ramc_we_ok <= 0;
+
+        readin_a_ok <= 1; // pulse
+        readin_b_ok <= 1;
+
+        done <= 1;        
+      end
+      STAT_S2_1 : begin
+        readin_a_ok <= 0; // pulse
+        readin_b_ok <= 0;
+      end
+      STAT_S3 : begin
+        rama_we_ok <= 0;
+        ramb_we_ok <= 0;
+      end
+      STAT_S3_1 : begin
+        cal_pulse   <= 1; // clearing index_c/counter/k
+
+      end
+    endcase
+  end
+end
+endmodule
