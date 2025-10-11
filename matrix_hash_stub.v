f40e7bb3c97a1b85216f062166ba01b271e08479
astelor <astelor000@gmail.com>
2025-10-11 21:45:07 +0800

    matrix hash stub script and verilog code

---
+++ matrix_hash_stub.v
@@ -0,0 +1,358 @@
+/*
+This is the hash stub module specifically for matrix A generation
+
+Since it's rather infeasible to cram two things together and try to make it work
+during prototyping stage.
+*/
+
+module matrix_hash_stub(
+  input clk,
+  input set,
+  input reset,
+
+  input readin,
+  input readout,
+  input full_in,
+  input [7:0] nonce, // manage externally for module coherence
+  input [7:0] matrix_hash_din,
+  input [7:0] in_index,
+
+  output [15:0] matrix_hash_dout_1, // goes into polyvec
+  output [15:0] matrix_hash_dout_2,
+  output [7:0]  out_index,
+
+  output readin_ok,
+  output done
+);
+
+// localparam DEPTH = 7; // 128 split in two
+// genvar i;
+// MEMORY BANKS =======================
+// RAM A (for input)
+wire       ram_a_we_1,   ram_a_we_2;
+wire [4:0] ram_a_addr_1, ram_a_addr_2; 
+wire [7:0] ram_a_din_1 , ram_a_din_2;
+wire [7:0] ram_a_dout_1, ram_a_dout_2;
+
+// RAM B (for output)
+wire        ram_b_we_1  , ram_b_we_2  ;
+wire [7:0]  ram_b_addr_1, ram_b_addr_2; 
+wire [15:0] ram_b_din_1 , ram_b_din_2 ;
+wire [15:0] ram_b_dout_1, ram_b_dout_2;
+
+dual_ram #(5, 8) ram_a(
+  .clk(clk),
+  .we_1  (ram_a_we_1  ),
+  .we_2  (ram_a_we_2  ),
+  .addr_1(ram_a_addr_1),
+  .addr_2(ram_a_addr_2),
+  .din_1 (ram_a_din_1 ),
+  .din_2 (ram_a_din_2 ),
+  .dout_1(ram_a_dout_1),
+  .dout_2(ram_a_dout_2)
+);
+
+dual_ram #(8, 16) ram_b( // two port output is possible
+  .clk(clk),
+  .we_1  (ram_b_we_1  ),
+  .we_2  (ram_b_we_2  ),
+  .addr_1(ram_b_addr_1),
+  .addr_2(ram_b_addr_2),
+  .din_1 (ram_b_din_1 ),
+  .din_2 (ram_b_din_2 ),
+  .dout_1(ram_b_dout_1),
+  .dout_2(ram_b_dout_2)
+);
+
+// FSM
+wire iscal;
+wire index_a_ctrl;
+wire index_b_ctrl;
+wire counter_ctrl;
+wire ram_a_we_ok_fsm;
+wire ram_b_we_ok_fsm;
+wire [7:0] counter_fsm;
+wire shake128_full_in;
+reg [7:0] shake128_done_read;
+wire shake128_done = shake128_done_read - 'd48;
+wire readin_ok_fsm;
+wire pulse;
+
+matrix_hash_stub_fsm fsm(
+  .clk(clk),
+  .set(set),
+  .reset(reset),
+
+  // INPUT
+  .full_in         (full_in), // from external
+  .counter         (counter_fsm),
+  .shake128_done   (shake128_done),
+  // OUTPUT
+  .iscal           (iscal),
+  .index_a_ctrl    (index_a_ctrl),
+  .index_b_ctrl    (index_b_ctrl),
+  .counter_ctrl    (counter_ctrl),
+  .ram_a_we_ok     (ram_a_we_ok_fsm),
+  .ram_b_we_ok     (ram_b_we_ok_fsm),
+  .shake128_full_in(shake128_full_in),
+  .pulse           (pulse),
+  .readin_ok       (readin_ok_fsm), // pulaw
+  
+  .done(done) // to external 
+);
+// INTERNAL REG BEGIN =========================//
+reg [4:0] index_a;
+reg [7:0] index_b;
+reg [7:0] counter;
+reg readin_ok_r;
+// INTERNAL REG END ===========================//
+
+// ASSIGN BEGIN ===============================//
+assign ram_a_we_1 = 0;
+assign ram_a_we_2 = ram_a_we_ok_fsm & readin_ok_r & readin;
+
+assign ram_a_addr_1 = (iscal) ? index_a : 0;  // yes cal
+assign ram_a_addr_2 = (iscal) ? 0 : in_index; // no cal
+
+assign ram_a_din_1 = 0; // read only
+assign ram_a_din_2 = (readin_ok) ? matrix_hash_din : 0;
+
+// TODO: umm ram_b_din and ram_a_dout not having any function is intended as a stub
+
+assign ram_b_we_1   = ram_b_we_ok_fsm;
+assign ram_b_we_2   = ram_b_we_ok_fsm;
+assign ram_b_addr_1 = index_b;
+assign ram_b_addr_2 = index_b + 1;
+assign ram_b_din_1  = 0;
+assign ram_b_din_2  = 0;
+
+assign matrix_hash_dout_1 = (done & readout) ? ram_b_dout_1 : 0;
+assign matrix_hash_dout_2 = (done & readout) ? ram_b_dout_2 : 0;
+
+assign out_index = (done & readout) ? (index_b - 2) >> 1: 0;
+assign counter_fsm = counter;
+assign readin_ok = readin_ok_r;
+
+// ASSIGN END =================================//
+integer fd1;
+integer fd2;
+integer fd3;
+
+// synthesis translate_off
+initial begin
+  fd1 = $fopen("D:/!Github_coding/project-kyber/sim_hash_stub/test-matrix_hash_stub.txt","w");
+  fd2 = $fopen("D:/!Github_coding/project-kyber/sim_hash_stub/matrix_hash.flag","w");
+  fd3 = $fopen("D:/!Github_coding/project-kyber/sim_hash_stub/matrix_hash2.flag","r");
+end
+// synthesis translate_on
+
+always @(posedge clk or posedge reset) begin
+  if(reset) begin
+    readin_ok_r <= 0;
+  end
+  else if(set) begin
+    if(readin_ok_fsm) begin
+      readin_ok_r <= 1;
+    end
+    else if(full_in) begin
+      readin_ok_r <= 0;
+    end
+  end
+  // readin_ok_r = (reset) ? 0 : ((readin_ok_r | readin_ok_fsm) & (~full_in));
+end
+
+always @(posedge clk or posedge reset) begin
+  if(reset) begin
+    $rewind(fd1);
+    $rewind(fd2);
+    index_a <= 0;
+    index_b <= 0;
+    counter <= 0;
+  end
+  else if(set) begin
+    if(index_a_ctrl) begin
+      index_a <= index_a + 1;
+    end
+    if(counter_ctrl) begin
+      counter <= counter + 1;
+    end
+    if(index_b_ctrl & done & readout) begin
+      index_b <= index_b + 2;
+    end
+    if(pulse) begin
+      $rewind(fd1);
+      index_a <= 0;
+      index_b <= 0;
+      counter <= 0;
+    end
+    $fwrite(fd2,"%b", shake128_full_in);
+    $rewind(fd2);
+    $fread(shake128_done_read, fd3, 0, 1);
+    $rewind(fd3);
+
+    if(shake128_done) begin
+      // synthesis translate_off
+      ram_b.load_mem(4);
+      // synthesis translate_on
+    end
+  end
+end
+
+// with the nonce in :>
+wire [7:0] stub_mem = (counter == 32) ? nonce : ram_a_dout_1;
+// stub logic, write the content of ram_a into a file
+always @(posedge clk) begin
+  if(set & iscal /*& (~pulse)*/) begin 
+    $fwrite(fd1,"%h\n", stub_mem);
+  end
+end
+
+task closefile;
+  begin
+    $fclose(fd1);
+    $fclose(fd2);
+    $fclose(fd3);
+  end
+endtask
+
+endmodule
+
+
+module matrix_hash_stub_fsm(
+  input clk,
+  input set,
+  input reset,
+
+  input full_in, // from external
+  input [7:0] counter,
+  input shake128_done,
+  output reg iscal,
+  
+  output reg index_a_ctrl,
+  output reg index_b_ctrl,
+  output reg counter_ctrl,
+
+  output reg ram_a_we_ok,
+  output reg ram_b_we_ok,
+  output reg shake128_full_in, // stub
+  output reg pulse, // this is used for resetting counters
+  output reg readin_ok, // pulse
+  output reg done // to external
+);
+
+localparam LOAD_S0   = 7;
+
+localparam STAT_S0   = 1;
+localparam STAT_S0_1 = 2;
+localparam STAT_S1   = 3;
+localparam STAT_S2   = 4;
+localparam STAT_S2_1 = 5;
+localparam STAT_S3   = 6;
+
+reg [7:0] next_state;
+reg [7:0] curr_state;
+
+always @(posedge clk or posedge reset) begin
+  if(reset) begin
+    curr_state <= LOAD_S0;
+  end
+  else if (set) begin
+    curr_state <= next_state;
+  end
+end
+
+always @(*) begin
+  if(set) begin
+    case (curr_state)
+      LOAD_S0 : begin
+        next_state = STAT_S0;
+      end
+      STAT_S0 : begin
+        if(full_in)
+          next_state = STAT_S0_1;
+        else
+          next_state = STAT_S0;
+      end 
+      STAT_S0_1 : begin
+        next_state = STAT_S1;
+      end
+      STAT_S1 : begin
+        if(counter == 31) // the timing for ram_a -> stub logic (python script)
+          next_state = STAT_S2;
+        else
+          next_state = STAT_S1;
+      end
+      STAT_S2 : begin
+        next_state = STAT_S2_1;
+      end
+      STAT_S2_1 : begin 
+        if(shake128_done) // the python script did the thing!
+          next_state = STAT_S3;
+        else
+          next_state = STAT_S2_1;
+      end
+      STAT_S3 : begin
+        next_state = LOAD_S0;
+      end
+      default : begin
+        $display("forbidden state");
+      end 
+    endcase
+  end
+end
+
+always @(posedge clk or posedge reset) begin
+  if(reset) begin
+    iscal <= 0;
+    index_a_ctrl <= 0;
+    index_b_ctrl <= 0;
+    counter_ctrl <= 0;
+    ram_a_we_ok  <= 0;
+    ram_b_we_ok  <= 0; // not assigned in states
+    readin_ok    <= 0;
+    shake128_full_in <= 0;
+    done <= 0;
+    pulse <= 0;
+  end
+  else if(set) begin
+    case (curr_state)
+      LOAD_S0 : begin
+        iscal <= 0;
+        ram_a_we_ok <= 1;
+        readin_ok <= 1;
+      end
+      STAT_S0 : begin
+        readin_ok <= 0;
+      end
+      STAT_S0_1 : begin
+        ram_a_we_ok <= 0;
+        index_a_ctrl <= 1;
+        index_b_ctrl <= 0;
+        done <= 0;
+        //pulse <= 1;
+      end
+      STAT_S1 : begin
+        //pulse <= 0;
+        counter_ctrl <= 1;
+        iscal <= 1;
+      end
+      STAT_S2 : begin
+        iscal <= 0;
+        index_a_ctrl <= 0;
+        counter_ctrl <= 0;
+        pulse <= 1;
+      end
+      STAT_S2_1 : begin
+        shake128_full_in <= 1;
+        pulse <= 0;
+      end
+      STAT_S3 : begin
+        shake128_full_in <= 0;
+        index_b_ctrl <= 1;
+        done <= 1;
+      end
+    endcase
+  end
+end
+
+endmodule
--- sim_hash_stub/matrix_script.py
+++ sim_hash_stub/matrix_script.py
@@ -1,25 +1,107 @@
 # SampleNTT stub
-from Crypto.Hash import SHAKE256
+import sys
+from time import sleep
+from Crypto.Hash import SHAKE128
 # this version from Cryptodome is more of a sponge function
+import string
 
-p = b'this is a test' # seed
 
-h = SHAKE256.new() # XOF.Init()
-h.update(p) # XOF.Absorb
+#p = b'this is a test' # seed
+def samplentt(p):
+    h = SHAKE128.new() # XOF.Init()
+    h.update(p) # XOF.Absorb
+    q = 3329
+    j = 0
+    a = [0]*256 # output array
+    while j < 256:
+        temp = h.read(3) # XOF.Squeeze(3)
+        d1 = temp[0] + (temp[1] & 0xf) << 8
+        d2 = (temp[1] >> 4) + (temp[2] << 4)
+        if d1 < q:
+            a[j] = d1
+            j = j + 1
+        if (d2 < q) and (j < 256):
+            a[j] = d2
+            j = j + 1
 
-q = 3329
-j = 0
-a = [0]*256 # output array
+    #print(a)
+    return a
 
-while j < 256:
-    temp = h.read(3) # XOF.Squeeze(3)
-    d1 = temp[0] + (temp[1] & 0xf) << 8
-    d2 = (temp[1] >> 4) + (temp[2] << 4)
-    if d1 < q:
-        a[j] = d1
-        j = j + 1
-    if (d2 < q) and (j < 256):
-        a[j] = d2
-        j = j + 1
+k = 0
+anim = ['-','\\','|','/']
 
-print(a)
+def func(ch: str) -> bool:
+    return ch not in string.hexdigits
+
+fd1 = open("./matrix_hash.flag","r") # shake_full_in
+fd2 = open("./matrix_hash2.flag","w") # shake_done
+fd3 = open("./test-matrix_hash_stub.txt","r") # the memory
+
+flag = 0
+flag2 = 0
+# gest = "it_nothing"
+counter = 0
+
+fd2.write(str(flag2))
+fd2.close()
+
+while 1:
+    temp = fd1.read()
+    fd1.seek(0)
+    # cleansing the flag
+    if(temp == '0' or temp == '1'):
+        flag = int(temp)&1
+    
+    if(flag == 1 and flag2 == 0):
+        seed = fd3.read() # data will be in hex
+        fd3.seek(0)
+        seed = seed.replace('\n','')
+        #print(seed)
+        #print(func(seed))
+        if(func(seed)):
+            mem  = open("./mem_matrix_hash_stub.hex","w") # the completed hash
+            seed = bytes.fromhex(seed)
+            matt = samplentt(seed)
+
+            #gest = gest.hex() # in hex
+            # put them in memory banks 32*4 
+            for i in matt:
+                mem.write(str(hex(i)[2:])) 
+                mem.write("\n")
+
+            flag2 = int(1)
+
+            fd2 = open("./matrix_hash2.flag","w") # shake_done
+            fd2.write(str(flag2))
+            fd2.close()
+            
+            mem.close()
+
+            counter = counter + 1
+            sys.stdout.write("\n")
+            sys.stdout.write("[+] message:\n");
+            sys.stdout.write("%s\n" % (seed.hex()));
+            sys.stdout.write("[+] hash:\n");
+            for r in range(len(matt)):
+                if(r%16==0):
+                    sys.stdout.write("%3d: " % (r>>4))
+
+                sys.stdout.write("%4s " % (matt[r]))
+                # if(r & 0x7 == 7):
+                #     sys.stdout.write(" ")
+                if(r & 0xf == 15):
+                    sys.stdout.write("\n")
+            sys.stdout.write("")
+        #else: 
+            
+    if(flag == 0 and flag2 == 1):
+        flag2 = 0
+        fd2 = open("./matrix_hash2.flag","w") # shake_done
+        fd2.write(str(flag2))
+        fd2.close()
+    
+    sys.stdout.write('\r')
+    sys.stdout.write("_[%s] hash stubbing [+] flag1 = %s. flag2 = %s. hash snippet: %s [%2d]" % (anim[k], flag, flag2 ,hex((matt[0]<<24)+(matt[1]<<12)+(matt[2])), counter))
+    k = (k + 1)%len(anim)
+    
+    sleep(0.25)
